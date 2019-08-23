d7d2de9d11a2050733baea450d419d1bb624f8c0
Keginold Philip <keginoldp@hotmail.com>
2019-08-23 17:17:01 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,5 +1,18 @@
 # require modules here
+ef load_library(path)
+  emoticons = YAML.load_file(path)
+  emoticon_hash = Hash.new
+  emoticon_hash["get emoticon"] = Hash.new
+  emoticon_hash["get meaning"] = Hash.new
+  # code goes here	  # code goes here
+  emoticons.each do |english_word, emoticon_set|
+    emoticon_hash["get emoticon"][emoticon_set.first] = emoticon_set.last
+    emoticon_hash["get meaning"][emoticon_set.last] = english_word
+  end
+  emoticon_hash
+end	end
 
+<<<<<<< HEAD
 def load_library(path)
   emoticons = YAML.load_file(path)
   emoticon_hash = Hash.new
@@ -18,4 +31,13 @@ end	end
 
 
  def get_english_meaning	def get_english_meaning
+=======
+
+ def get_japanese_emoticon	def get_japanese_emoticon
+@@ -10,4 +20,4 @@ def get_japanese_emoticon
+
+
+ def get_english_meaning	def get_english_meaning
+  # code goes here	  # code goes here
+>>>>>>> e49d41815276b80bdae9c90477e08c2be3505f1a
 end 	end
